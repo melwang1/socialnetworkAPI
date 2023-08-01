9faa392c79ff953df7b05de5e86e7126c3383ce8
Melanie Wang <melaniiewang@hotmail.com>
2023-08-01 14:59:41 -0400

    added models user.js and started on the reaction and though.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: [true, 'Email address must be valid'],
+        unique: true,
+        validate: {
+            validator: function (v){
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
+        }
+                  }
+      },
+      thoughts: [{
+          type: Schema.Types.ObjectId,
+          ref: 'thoughts'
+      }],
+      friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user' 
+      }],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id:false
+      }
+)
+
+userSchema.virtual('friendCount').get(function(){
+        return this.friends.length
+    })
+
+const User = model('user', userSchema);
+
+module.exports = User;
